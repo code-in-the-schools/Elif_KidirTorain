ef590e0ee0401b8d97d9e78cad7c3f84e2c8986f
Kidir Torain <kktorainnn@gmail.com>
2020-07-22 17:20:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+found = False
+
+n = random.randint(-100,100) 
+i = int(input("Enter a number..."))
+if n > 
+while found == True:
+  print("Congrats...")
+  while found == False 
+  print("choose another number...")
+
